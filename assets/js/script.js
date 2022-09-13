15920ca27cfc4d59c367c01ed113b0022efddf53
sanam1060 <sanam_g1990@yahoo.com>
2022-09-13 09:06:16 -0400

    connect the css and  html files, add time-block class to <div>s in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,16 @@ $("document").ready(function () {
   const today = moment();
   $("#currentDay").text(today.format("dddd" + ", " + "MMMM Do YYYY"));
 
-let currentTime = moment().hours() + 3;
-console.log(currentTime);
+// Add event listeners to buttons to save data in localStorage 
+$(".saveBtn").on("click", function () {
+  var hour = $(this).parent().attr("id");
+  var event = $(this).siblings(".description").val();
+  localStorage.setItem(hour, event);
+});
 
+let currentTime = moment().hours();
+
+// Bakcground colors
 inputEl = $("textarea").each(function () {
   var textEL = $(this).attr("id");
   console.log(textEL);
@@ -17,28 +24,21 @@ inputEl = $("textarea").each(function () {
   } else if (textEL > currentTime) {
     $(this).addClass("future");
   };
-
+  
   setInterval(function () {
     inputEl;
   }, 1000);
 });
 
-  $(".saveBtn").on("click", function () {
-    var hour = $(this).parent().attr("id");
-    var event = $(this).siblings(".description").val();
-    localStorage.setItem(hour, event);
-  });
-  
-  // Data is retrieved from localStorage even after refreshing the page
-  $("#9am .description").val(localStorage.getItem("9am"));
-  $("#10am .description").val(localStorage.getItem("10am"));
-  $("#11am .description").val(localStorage.getItem("11am"));
-  $("#12pm .description").val(localStorage.getItem("12pm"));
-  $("#1pm .description").val(localStorage.getItem("1pm"));
-  $("#2pm .description").val(localStorage.getItem("2pm"));
-  $("#3pm .description").val(localStorage.getItem("3pm"));
-  $("#4pm .description").val(localStorage.getItem("4pm"));
-  $("#5pm .description").val(localStorage.getItem("5pm"));
+// Data is retrieved from localStorage even after refreshing the page
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
 
-  //displayTimeColor();
 });
